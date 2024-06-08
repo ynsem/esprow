46787764f3d2899a950d277fa6d875fe3084020c
ynsem <y.n.semenchenko@mail.ru>
2024-06-08 19:26:51 +0300

    template

---
+++ src/types.ts
@@ -0,0 +1,11 @@
+export type User = {
+    id: string
+    isActive: boolean
+    picture: string
+    age: number
+    name: string
+    email: string
+    address: string
+    about: string
+    registered: string
+}
